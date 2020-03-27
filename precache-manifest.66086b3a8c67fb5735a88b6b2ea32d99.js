6df85eca246ea2d12850c5a1f7e6dfb5471f0d6b
Aashis Kishore <aashiskishore@gmail.com>
2020-03-27 12:46:13 +0530

    New build

---
+++ precache-manifest.66086b3a8c67fb5735a88b6b2ea32d99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43765256e830fed4275ce3e12a3d1553",
+    "url": "./index.html"
+  },
+  {
+    "revision": "679c8d7d45bce7a541f1",
+    "url": "./static/css/main.caf1db2f.chunk.css"
+  },
+  {
+    "revision": "fcb1029d74144164bee3",
+    "url": "./static/js/2.9632fcdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.9632fcdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679c8d7d45bce7a541f1",
+    "url": "./static/js/main.dcf262b7.chunk.js"
+  },
+  {
+    "revision": "419c4c737d9562548096",
+    "url": "./static/js/runtime-main.940340bb.js"
+  }
+]);
